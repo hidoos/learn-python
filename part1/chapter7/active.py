0cc7b48708e90c5d20a14a45bde7a5309905eb23
hidoos <sk8huaban@gmail.com>
2016-07-30 23:06:21 +0800

    add case: use while move item of list

---
+++ part1/chapter7/active.py
@@ -0,0 +1,10 @@
+prompt = "\n Tell me something, and I will repeat it back to you:"
+prompt += "\n Eneter 'quit' to end the program "
+active = True
+
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else: 
+        print(message)

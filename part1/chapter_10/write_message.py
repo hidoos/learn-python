3d18b6410ebe989643288d1800f091396b342e2a
hidoos <sk8huaban@gmail.com>
2016-08-02 11:17:30 +0800

    完成写入的文件的demo case

---
+++ part1/chapter_10/write_message.py
@@ -0,0 +1,6 @@
+filename = 'programming.txt'
+
+with open(filename, 'a') as file_object:
+    file_object.write('I also love find meaning in large dataset!\n')
+    file_object.write('I love creating apps that can runing in brower!\n')
+

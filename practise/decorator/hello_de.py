59a41b8373cd01ff5dc753b5ddf143e04a82124e
hidoos <sk8huaban@gmail.com>
2016-11-24 15:27:28 +0800

    add decorator demo

--- practise/decorator/hello_de.py
+++ practise/decorator/hello_de.py
@@ -1,16 +1,22 @@
-def check_is_admin(f):
-    def wrapper(*args, **kwargs):
-        print(args)
-        print(kwargs)
-        if kwargs.get('username') != 'admin':
-            raise Exception("This user is not allowed to get food")
-        return f(*args, **kwargs)
-    return wrapper
+import functools
+def logging(func):
+    @functools.wraps(func)
+    def decorator(*args, **kwargs):
+        print("%s called" % func.__name__)
+        result = func(*args, **kwargs)
+        print("%s end" % func.__name__)
+        return result
+    return decorator
 
-@check_is_admin
-def get_food(*args, **kwargs):
-    print('hello')
-    pass
+@logging
+def test01(a, b):
+    print("in function test01, a=%s, b=%s" % (a, b))
+    return 1
 
-get_food('hidoos', 'admin', 'age', username = 'admin')
+@logging
+def test02(a, b, c=1, **kwargs):
+    print("in function test02, a=%s, b=%s, c=%s" % (a, b, c))
+    return 1
 
+print(test01(1, 2))
+print(test02(1, 2, c=3, d=4))
